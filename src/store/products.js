8c69be5694ceedf9dbd15d33ca50eb0919d1be09
Joshua Sandoval <101337266+Juswa031@users.noreply.github.com>
2025-04-05 13:41:07 +0800

    integrate api for product

--- src/store/products.js
+++ src/store/products.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'https://cdn.jsdelivr.net/npm/vue3-toastify@0.1.11/+esm';
 import 'vue3-toastify/dist/index.css';
 
-const endpoint = "https://momecoph-api.shop/api/consumer/v1"
+const endpoint = "https://momecoph-api.shop/api/admin/v1"
 
 const state = {
     products: null,
@@ -18,6 +18,49 @@ const actions = {
             const response = await axios.get(`${endpoint}/products?per_page=${filter.per_page}&order_by=${filter.order_by}&sort_by=${filter.sort_by}`
             );
             await commit('UPDATE_PRODUCTS', response.data);
+        } catch (error) {
+            this.errors = error.response.data.errors;
+            var errorlist = "";
+            this.errors.forEach(element => {
+                errorlist += element.detail + "\n";
+            });
+            toast.error(errorlist)
+        }
+    },
+    async ADD_PRODUCT({ commit }, payload) {
+        try {
+            const response = await axios.post(`${endpoint}/products`, payload);
+            toast.success("Product Added successfully")
+            return response.data;
+        } catch (error) {
+            this.errors = error.response.data.errors;
+            var errorlist = "";
+            this.errors.forEach(element => {
+                errorlist += element.detail + "\n";
+            });
+            toast.error(errorlist)
+        }
+    },
+    async UPDATE_PRODUCT({ commit }, payload) {
+        try {
+            const response = await axios.put(`${endpoint}/products/${payload.id}`, payload);
+            toast.success("Product Updated successfully")
+            return response.data;
+        } catch (error) {
+            this.errors = error.response.data.errors;
+            var errorlist = "";
+            this.errors.forEach(element => {
+                errorlist += element.detail + "\n";
+            });
+            toast.error(errorlist)
+        }
+    },
+    async REMOVE_PRODUCT({ commit }, product_id) {
+        try {
+            const response = await axios.delete(`${endpoint}/products/${product_id}`);
+            toast.success("Product deleted successfully")
+            return response.data;
+
         } catch (error) {
             this.errors = error.response.data.errors;
             var errorlist = "";
